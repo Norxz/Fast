00494434800d194d9636d49e5d7775618e93d48a
Andres Espinosa <espinosalozano6819@gmail.com>
2025-05-26 01:28:24 -0500

    Prueba despliegue

--- backend/src/main/java/com/fast/security/config/WebSecurityConfig.java
+++ backend/src/main/java/com/fast/security/config/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig {
                 "http://localhost:3000",
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
-                "https://fast-pi-three.vercel.app" // <--- AGREGA AQUÍ TU URL DE VERCEL
+                "https://serviexpress.vercel.app/"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
