ccef59fa3f6f6a5b08b3a9f19b9967b778eec770
Andres Espinosa <espinosalozano6819@gmail.com>
2025-06-04 20:23:32 -0500

    Email estado de solicitud

--- backend/src/main/java/com/fast/service/EmailService.java
+++ backend/src/main/java/com/fast/service/EmailService.java
@@ -6,6 +6,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import com.fast.domain.Solicitud;
 
 import com.itextpdf.io.source.ByteArrayOutputStream;
 import com.itextpdf.kernel.pdf.PdfDocument;
