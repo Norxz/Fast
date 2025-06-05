c32cee81d30bb9f4581e1170fc9562a4151f0ed0
Andres Espinosa <espinosalozano6819@gmail.com>
2025-06-05 09:22:23 -0500

    Pruebas Contactenos

--- fast-frontend/js/index.js
+++ fast-frontend/js/index.js
@@ -1,16 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('contactForm');
-
-    if (!form) {
-        console.log('Formulario no encontrado.');
-        return;
-    }
-
-    form.addEventListener('submit', (e) => {
-        e.preventDefault(); // Evita el envío real del formulario
-        console.log('¡Formulario enviado!');
-    });
-
     const button = document.getElementById('submitButton');
     if (button) {
         button.addEventListener('click', () => {
