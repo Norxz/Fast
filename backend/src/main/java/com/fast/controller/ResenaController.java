1f2c3d3aab0f2c9377a6eee79cd22a3f0252bf07
nightwing919 <andresespinosa156@gmail.com>
2025-06-09 21:02:40 -0500

    Resenas

--- backend/src/main/java/com/fast/controller/ResenaController.java
+++ backend/src/main/java/com/fast/controller/ResenaController.java
@@ -9,6 +9,7 @@ import com.fast.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 import java.util.Map;
